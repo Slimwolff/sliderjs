894a132141ed212383c41b14a38f7fbf22d786ad
SlimWolff <ptudoandtodos@gmail.com>
2021-08-17 13:39:48 -0300

    refactoring methods and optiminzing functions

--- init.js
+++ init.js
@@ -2,11 +2,8 @@ let cardCont = document.querySelector('.card-container');
 
 let cards = cardCont.children;
 let posSlide = 1;
-
-let width = cardCont.offsetWidth,
-    height = cardCont.offsetHeight,
-    cardWidth = cards[0].offsetWidth,
-    cardHeight = cards[0].offsetHeight;
+let subtract = 0;
+let width = cardCont.offsetWidth;
 
 
 // return a string with rgba color - its just to view cards better
@@ -23,91 +20,69 @@ for(let i=0;i<cards.length;i++){
     cards[i].style.background = rgba();
 }
 
-function positionElements(){
-
+async function positionElements(){
+    
     for(let i=0; i<cards.length; i++){
-
-        if(i == 0) {
+        if(i == 0){
             centerElement(cards[i]);
-        }else if(i == 1) {
-            setRightElement(cards[i])
-        }else if (i > 1){
-            let e = cards[i];
-            let eleAfter = cards[i-1]
-            setRightElementAfter(cards[i],eleAfter);
+            let widEle = cards[i].offsetWidth;
+            subtract = subtractValue(widEle);
+            console.log(subtract)
+        }else if(i > 0){
+            let leftInit = cards[i-1].offsetLeft;
+            let widthElement = cards[i-1].offsetWidth;
+            setPosElement(cards[i],leftInit,widthElement);
         }
     }
+    
 }
 
 function backwardSlide(){
-    let currentCard = posSlide-1;
-    let backSlide = currentCard-1;
-    let forSlide = currentCard+1;
-    
-    if(posSlide !== 1){
-        
-        setRightElement(cards[currentCard]);
-        centerElement(cards[backSlide]);
-        if(posSlide !== cards.length){
-            setRightElementAfter(cards[forSlide],cards[forSlide]);
-        }
-        
-        
-        posSlide--;
-    }
-    
 
+    let c = cards;
+
+    if(posSlide > 1){
+        for(let i=0; i<c.length; i++){
+            let offLeft = cards[i].offsetLeft;
+            c[i].style.left = `${offLeft+subtract}px`;
+        }
+        posSlide--;    
+    } 
 }
 
 function forwardSlide(){
-    
-    let a = posSlide-1;
-    let b = posSlide;
-    let c = posSlide+1;
-    
-    console.log(`slide position: ${posSlide}, cards length: ${cards.length}`);
-    console.log(`a: ${a}, b: ${b}, c: ${c} - posSlide: ${posSlide}`);
-    if(posSlide <= cards.length){
+    let c = cards;
 
-        if(posSlide !== cards.length){
-
-            if(posSlide !== 1){
-                setLeftElementAfter(cards[a],cards[a]);
-            }
-            
-            setLeftElement(cards[a]);
-            centerElement(cards[b]);
-
-            if(c == cards.length){
-
-            }else{
-                setRightElement(cards[c]);
-            }
-            posSlide++;
-            console.log(`position Slide on increment: ${posSlide}`);
+    if(posSlide < c.length){
+        for(let i=0; i<c.length; i++){
+            let offLeft = cards[i].offsetLeft;
+            c[i].style.left = `${offLeft-subtract}px`;
         }
-            
-        
-    }
+        posSlide++;
+    } 
+    
 }
 
-function setLeftElement(e){
-    e.style.left = `-${e.offsetWidth - ( e.offsetWidth * 0.2 )}px`;
+function setLeftElement(e,value){
+    e.style.left = `${value}px`;
 }
+function subtractValue(width){
+    return width+(width*0.365);
+}
+function getValuePos(init,width){
+    return (init+width)+(width*0.365);
+}
+
+function setPosElement(e,init,width){
+    e.style.left = `${getValuePos(init,width)}px`
+    console.log(`init: ${init} + width: ${width} + percentageWidth: ${width*0.365}`);
+}
+
 function centerElement(e){
     e.style.left = `${ (width / 2) - (e.offsetWidth / 2) }px`;
     console.log(e.offsetWidth);
 }
 
-function setRightElement(e){
-    e.style.left = `${(width-(e.offsetWidth*0.2))}px`;
-}
-function setLeftElementAfter(element,elementAfter){
-    element.style.left = `-${elementAfter.offsetLeft - element.offsetWidth}px`;
-}
-function setRightElementAfter(element,elementAfter){
-    element.style.left = `${elementAfter.offsetLeft + element.offsetWidth}px`
-}
 
 
 positionElements();
